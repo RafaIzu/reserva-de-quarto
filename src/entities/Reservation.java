e93fc041697d56f3aab83093d4c8d33b95fcba10
RafaIzu <bomizukawa@gmail.com>
2020-01-09 15:56:58 -0300

    Segunda Versao

--- src/entities/Reservation.java
+++ src/entities/Reservation.java
@@ -1,11 +1,16 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 	private Integer roomNumber;
 	private Date checkin;
 	private Date checkout;
+	
+	private static SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
+	
 	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
 		this.roomNumber = roomNumber;
 		this.checkin = checkin;
@@ -23,6 +28,29 @@ public class Reservation {
 
 	public Date getCheckout() {
 		return checkout;
+	}
+	
+	public long duration() {
+		long diff=checkout.getTime()-checkin.getTime();
+		return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
+	}
+	public void updateDates(Date checkin, Date checkout) {
+		this.checkin=checkin;
+		this.checkout=checkout;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room"
+				+roomNumber
+				+", check-in: "
+				+sdf.format(checkin)
+				+", check-out: "
+				+sdf.format(checkout)
+				+", "
+				+duration()
+				+"nights";
+				
 	}
 
 	
